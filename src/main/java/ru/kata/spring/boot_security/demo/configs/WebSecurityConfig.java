dcb98d1bd68b695b0b48af07d19ba2ba5a219dbd
Alexei <splash.dll@gmail.com>
2023-05-31 10:27:48 +0300

    some experiments with db in docker

--- src/main/java/ru/kata/spring/boot_security/demo/configs/WebSecurityConfig.java
+++ src/main/java/ru/kata/spring/boot_security/demo/configs/WebSecurityConfig.java
@@ -53,6 +53,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .csrf().disable()
                 .authorizeRequests()
                 .antMatchers("/api/admin").hasRole("ADMIN")
+                .antMatchers("/api/admin/users").hasRole("ADMIN")
                 .antMatchers("/admin").hasRole("ADMIN")
                 .antMatchers("/user").hasAnyRole("USER", "ADMIN")
                 .antMatchers("api/user").hasAnyRole("USER", "ADMIN")
